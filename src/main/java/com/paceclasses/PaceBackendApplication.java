6b9a407eb20e97ae90423732df274e09b64e8dcf
Kripton0810 <subhankar0810@gmail.com>
2022-09-17 03:46:02 +0530

    init

---
+++ src/main/java/com/paceclasses/PaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.paceclasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaceBackendApplication.class, args);
+	}
+
+}
