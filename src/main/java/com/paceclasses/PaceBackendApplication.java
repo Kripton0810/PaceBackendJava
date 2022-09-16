a4303a4ba29525c6557a23f18bb648658e78f3c1
Kripton0810 <subhankar0810@gmail.com>
2022-09-17 03:53:31 +0530

    as

--- src/main/java/com/paceclasses/PaceBackendApplication.java
+++ src/main/java/com/paceclasses/PaceBackendApplication.java
@@ -8,7 +8,7 @@ public class PaceBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaceBackendApplication.class, args);
-		//comment hello command
+		//comment hello command as
 	}
 
 }
